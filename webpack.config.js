31cbc74ef83e2632a4839ae9d8c6abf1f4369424
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-07-29 22:03:48 +0800

    Completed Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,30 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Weather Report',
       template: './src/index.html'
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
         {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
         },
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+        
     ],
   },
 };
